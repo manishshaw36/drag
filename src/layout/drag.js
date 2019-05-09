9bcb187a420b81836872ab1083869d50cf8085cb
manishshaw36 <manishshaw36@gmail.com>
2019-05-09 16:37:48 +0530

    first

---
+++ src/layout/drag.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+
+class Drag extends Component {
+
+    // Normally you would want to split things out into separate components.
+    // But in this example everything is just done in one place for simplicity
+    render() {
+        const { getListStyle, getItemStyle, droppableId } = this.props
+        return (
+            <Droppable droppableId={droppableId}>
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        style={getListStyle(snapshot.isDraggingOver)}>
+                        {this.state.items.map((item, index) => (
+                            <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}>
+                                {(provided, snapshot) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={getItemStyle(
+                                            snapshot.isDragging,
+                                            provided.draggableProps.style
+                                        )}>
+                                        {item.content}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        );
+    }
+}
+
+export default Drag;
